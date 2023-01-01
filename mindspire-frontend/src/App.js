8b33fbe197e29dff4533fe22dfd39dc3b1462b38
You-now-Who <yashyadav.sr@gmail.com>
2023-01-01 13:52:01 +0530

    Frontend changes

--- mindspire-frontend/src/App.js
+++ mindspire-frontend/src/App.js
@@ -1,23 +1,39 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import Layout from './components/Layout'
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './components/Main';
+
 
 function App() {
-  
-  const [backendData, setBackendData] = useState([])
 
-  useEffect(() => {
-    fetch('/api')
-      .then(data => {{
-        setBackendData(data)
-      }})
-  }, [])
+  // const [backendData, setBackendData] = useState([])
+
+  // useEffect(() => {
+  //   fetch('/api')
+  //     .then(data => {
+  //       {
+  //         setBackendData(data)
+  //       }
+  //     })
+  // }, [])
 
   return (
-    <div>
-      
-      {/* {{(typeof backendData.users == 'undefined') ? 'Loading...' : backendData.users}} */}
-
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+        {/* <Route exact path="/" element={<Layout />}> */}
+          <Route exact path='/' element={<Layout />} />
+          <Route path='/main' element={<Main />} />
+          {/* <Route path="blogs" element={<Blogs />} /> */}
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
+
   )
 }
 
 export default App
+
---
+++ mindspire-frontend/src/components/Footer.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div className="container py-5">
+            {/* <div className="row g-5">
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="text-white mb-3">Quick Link</h4>
+                    <a className="btn btn-link" href="">About Us</a>
+                    <a className="btn btn-link" href="">Contact Us</a>
+                    <a className="btn btn-link" href="">Privacy Policy</a>
+                    <a className="btn btn-link" href="">Terms & Condition</a>
+                    <a className="btn btn-link" href="">FAQs & Help</a>
+                </div>
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="text-white mb-3">Contact</h4>
+                    <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>Hogwarts, Scottland</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+111111 11111</p>
+                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>Mindspire@Mindspire.com</p>
+                    <div className="d-flex pt-2">
+                        <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-twitter"></i></a>
+                        <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-facebook-f"></i></a>
+                        <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-youtube"></i></a>
+                        <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="text-white mb-3">Gallery</h4>
+                    <div className="row g-2 pt-2">
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-1.jpg" alt=""/>
+                        </div>
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-2.jpg" alt=""/>
+                        </div>
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-3.jpg" alt=""/>
+                        </div>
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-2.jpg" alt=""/>
+                        </div>
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-3.jpg" alt=""/>
+                        </div>
+                        <div className="col-4">
+                            <img className="img-fluid bg-light p-1" src="img/course-1.jpg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+        </div>
+        <div className="container">
+            <div className="copyright">
+                <div className="row">
+                    <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a className="border-bottom" href="#">Mindspire</a>, All Right Reserved.
+                        Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a><br/><br/>
+                        Distributed By <a className="border-bottom" href="https://themewagon.com">ThemeWagon</a>
+                    </div>
+                    <div className="col-md-6 text-center text-md-end">
+                        <div className="footer-menu">
+                            <a href="">Home</a>
+                            <a href="">Cookies</a>
+                            <a href="">Help</a>
+                            <a href="">FQAs</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
